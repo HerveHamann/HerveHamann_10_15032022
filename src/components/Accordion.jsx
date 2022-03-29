1a06bc438a2ebb8a690a52fd04574b631bf43f8c
Herve Hamann <hervehamann31@gmail.com>
2022-03-29 12:43:15 +0200

    Corrections suite à soutenance

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -2,19 +2,26 @@ import React, { useState } from "react";
 import arrowdown from "../assets/img/arrowdown.svg";
 import arrowup from "../assets/img/arrowup.svg";
 
-const Accordion = ({ header, description, equipments }) => {
+const Accordion = ({ header, description, text, equipments }) => {
   const [opened, setOpened] = useState(false);
 
-  let content = header === "Description" ? description : equipments;
+  let content = "";
+  if (header === "Description") {
+    content = description;
+  } else if (header === "Equipements") {
+    content = equipments;
+  } else {
+    content = text;
+  }
 
   return (
-    <div className="accordion">
+    <div className={content === text ? "aboutaccordion" : "accordion"}>
       <div className="title" onClick={() => (opened ? setOpened(false) : setOpened(true))}>
         <p>{header}</p>
         <img src={opened ? arrowdown : arrowup} alt="arrow" className="arrowupdown" />
       </div>
       <div className={opened ? "container" : "container-closed"}>
-        {description
+        {description || text
           ? content
           : content.map((item, index) => (
               <span key={index}>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Aboutaccordion from "../components/Aboutaccordion";
+import Accordion from "../components/Accordion";
 import Footer from "../components/layout/Footer";
 import Header from "../components/layout/Header";
 
@@ -11,19 +11,19 @@ const About = () => {
         <div className="aboutbanner"></div>
       </div>
       <div className="aboutinfo">
-        <Aboutaccordion
+        <Accordion
           header="Fiabilité"
           text="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
         />
-        <Aboutaccordion
+        <Accordion
           header="Respect"
           text="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
         />
-        <Aboutaccordion
+        <Accordion
           header="Service"
           text="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
         />
-        <Aboutaccordion
+        <Accordion
           header="Sécurité"
           text="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
         />
